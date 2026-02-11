0d0b53d5c1f6e100a1e4b125b351addb23396df5
Rodrigo Costa <rodrigojesusdacosta@gmail.com>
2026-02-11 19:23:29 -0300

    feat: add functional theme toggle button

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,15 @@
 const html = document.documentElement
-const themeToggleButton = document.querySelector('')
+const themeToggleButton = document.querySelector('.sliderContainer')
+
+themeToggleButton.addEventListener('click', function(){
+
+    if (html.getAttribute('data-theme') === 'dark'){
+        html.removeAttribute('data-theme')
+        themeToggleButton.classList.remove('sliderContainer--dark')
+
+    } else{
+        html.setAttribute('data-theme', 'dark')
+        themeToggleButton.classList.add('sliderContainer--dark')
+    }
+
+})
