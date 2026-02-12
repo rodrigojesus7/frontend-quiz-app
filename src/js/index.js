65f46faa08281e1c83b3813eed24a6e5856944e9
Rodrigo Costa <rodrigojesusdacosta@gmail.com>
2026-02-12 15:32:17 -0300

    refactor

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,49 @@
 const html = document.documentElement
 const themeToggleButton = document.querySelector('.sliderContainer')
 
-themeToggleButton.addEventListener('click', function(){
+const htmlQuizzButton = document.querySelector('#htmlQuizz')
+const cssQuizzButton = document.querySelector('#cssQuizz')
+const javascriptQuizzButton = document.querySelector('#javascriptQuizz')
+const accessibilityQuizzButton = document.querySelector('#accessibilityQuizz')
 
-    if (html.getAttribute('data-theme') === 'dark'){
+const buttons = [htmlQuizzButton, cssQuizzButton, javascriptQuizzButton, accessibilityQuizzButton]
+
+
+const startMenu = document.querySelector('.startMenu')
+const quizzSection = document.querySelector('.quizzSection')
+const scoreSection = document.querySelector('.scoreSection')
+
+const headerSectionTitle = document.querySelector('.header__titleContainer')
+
+
+
+themeToggleButton.addEventListener('click', function () {
+
+    if (html.getAttribute('data-theme') === 'dark') {
         html.removeAttribute('data-theme')
         themeToggleButton.classList.remove('sliderContainer--dark')
 
-    } else{
+    } else {
         html.setAttribute('data-theme', 'dark')
         themeToggleButton.classList.add('sliderContainer--dark')
     }
 
 })
+
+
+
+buttons.forEach(button => {
+    button.addEventListener('click', function () {
+
+        startMenu.classList.add('hidden');
+        quizzSection.classList.remove('hidden');
+
+        headerSectionTitle.classList.remove('invisible')
+
+        if(button.classList.contains = 'HTML'){
+        }
+    })
+})
+
+
+
